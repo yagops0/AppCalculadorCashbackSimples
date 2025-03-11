14e152974b8128f97da38c382cf0af59b6908cd8
yagops0 <yagosalvador34@gmail.com>
2025-03-10 21:59:02 -0300

    Continuando o trabalho

--- app/src/main/java/com/example/appcalculadoracashback/HomeActivity.java
+++ app/src/main/java/com/example/appcalculadoracashback/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.appcalculadoracashback;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,27 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+        btnVoltar.setOnClickListener(this);
+
+        Button btnContinuar = findViewById(R.id.btnContinuar);
+        btnContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent telaProcessar = new Intent(HomeActivity.this, ProcessarActivity.class);
+                startActivity(telaProcessar);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnVoltar){
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/appcalculadoracashback/ProcessarActivity.java
+++ app/src/main/java/com/example/appcalculadoracashback/ProcessarActivity.java
@@ -1,6 +1,10 @@
 package com.example.appcalculadoracashback;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ProcessarActivity extends AppCompatActivity {
+public class ProcessarActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public double calcular(double valor){
+        if (valor <= 100){
+            valor = valor * 0.03;
+            return valor;
+        } else if (valor > 100 && valor <= 100) {
+            valor = valor * 0.05;
+            return valor;
+        } else if (valor > 1000 && valor <= 6000) {
+            valor = valor * 0.07;
+            return valor;
+        } else if (valor > 6000) {
+            valor = valor * 0.1;
+            return valor;
+        }
+
+        return 0.0;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +41,55 @@ public class ProcessarActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+
+
+
+
+
+        Button btnCalcular = findViewById(R.id.btnCalcular);
+        btnCalcular.setOnClickListener(this);
+
+        Button btnConcluir = findViewById(R.id.btnConcluir);
+        btnConcluir.setOnClickListener(this);
+
+
+    }
+    EditText etValor = findViewById(R.id.etValor);
+    double valor = Double.parseDouble(etValor.getText().toString());
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnConcluir){
+            finish();
+        } else if (v.getId() == R.id.btnCalcular) {
+            if (valor <= 100.0){
+                TextView textPercent = findViewById(R.id.tvPorcentagem);
+                textPercent.setText("A porcentagem do cashback é 3%!!");
+
+                TextView textValorCash = findViewById(R.id.tvValor);
+                textValorCash.setText("O valor do seu cashback é R$ " + calcular(valor) + ".");
+            } else if (valor > 100 && valor <= 1000) {
+                TextView textPercent = findViewById(R.id.tvPorcentagem);
+                textPercent.setText("A porcentagem do cashback é 5%!!");
+
+                TextView textValorCash = findViewById(R.id.tvValor);
+                textValorCash.setText("O valor do seu cashback é R$ " + calcular(valor) + ".");
+            } else if (valor > 1000 && valor <= 6000) {
+                TextView textPercent = findViewById(R.id.tvPorcentagem);
+                textPercent.setText("A porcentagem do cashback é 7%!!");
+
+                TextView textValorCash = findViewById(R.id.tvValor);
+                textValorCash.setText("O valor do seu cashback é R$ " + calcular(valor) + ".");
+            } else if (valor > 6000) {
+                TextView textPercent = findViewById(R.id.tvPorcentagem);
+                textPercent.setText("A porcentagem do cashback é 10%!!");
+
+                TextView textValorCash = findViewById(R.id.tvValor);
+                textValorCash.setText("O valor do seu cashback é R$ " + calcular(valor) + ".");
+            }
+
+        }
     }
 }
